b09fdc4a5e177c08a958403fb5e2d140771d7f16
Bintangr191 <bintangrachman27@gmail.com>
2025-04-29 00:59:56 +0700

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,23 @@ btn.addEventListener('click', () => {
   }, 2000);
 });
 
+// Typing Effect
 const desc = document.getElementById('desc');
 const text = "Pelajari komponen komputer dengan teknologi Augmented Reality!";
+let index = 0;
 
-desc.textContent = text;
+function typeWriter() {
+  if (index < text.length) {
+    desc.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 50); 
+  }
+}
+
+window.addEventListener('load', () => {
+  setTimeout(typeWriter, 500); 
+  const teamMembers = document.querySelectorAll('.team-member');
+  teamMembers.forEach(member => {
+    member.classList.add('show'); 
+  });
+}); 
